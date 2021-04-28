474065e4392434ec5b47ed834973769825e4728a
Beni Verner <binitto10@gmail.com>
2021-04-28 16:10:32 -0700

    structs RequestResult and RequestInfo added

--- IRequestHandler.h
+++ IRequestHandler.h
@@ -1,4 +1,21 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <ctime>
+
+struct RequestResult
+{
+	std::vector<unsigned char> Buffer;
+	IRequestHandler* newHandler;
+};
+
+struct RequestInfo
+{
+	std::string requestId;
+	time_t receivalTime;
+	std::vector<unsigned char> buffer;
+};
+
 class IRequestHandler
 {
 	// abstract class for all request handlers, will be empty in current version(1.0.1)
