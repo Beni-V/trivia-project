0578ce1930ae725ef2d2608b0e6522e15cb32100
Beni Verner <binitto10@gmail.com>
2021-05-05 18:25:16 -0700

    Constructor for qlite databse implemented

--- SqliteDatabase.cpp
+++ SqliteDatabase.cpp
@@ -1,11 +1,42 @@
 #include "SqliteDatabase.h"
 
+SqliteDatabase::SqliteDatabase()
+{
+    std::string dbFileName = "MyDB.sqlite";
+
+    int doesFileExist = _access(dbFileName.c_str(), 0); // check if db is already exist (before trying to create)
+
+    int res = sqlite3_open(dbFileName.c_str(), &db); // open the db
+    if (res != SQLITE_OK) // if failed to open db
+    {
+        db = nullptr;
+        std::cout << "Failed to open DB" << std::endl;
+    }
+    else // if not failed
+    {
+        std::cout << "DB opened succsessfully!\n";
+    }
+
+
+    if (doesFileExist == 0) // if DB elready exist
+    {
+        std::cout << "DB already exist\n";
+    }
+
+    else // if not, create the tables
+    {
+        // create users table
+        std::string sqlStatement = "CREATE TABLE USERS (USERNAME TEXT, PASSWORD TEXT, EMAIL TEXT); ";
+        sendSqlStatement(sqlStatement, nullptr, nullptr);
+
+    }
+}
+
 /*
 input: sqlStatement that should be executed, callback function (may be nullptr), data for output from callback (also may be nullptr)
 output: null
 function will execute sql statement and return result from callback to the third parameter if second and third parameters are not null
 */
-
 void SqliteDatabase::sendSqlStatement(std::string sqlStatement, int(*callback)(void*, int, char**, char**), void* data)
 {
     char* errMessage = nullptr;
@@ -13,3 +44,4 @@ void SqliteDatabase::sendSqlStatement(std::string sqlStatement, int(*callback)(v
     if (res != SQLITE_OK)
         std::cout << "Trouble with sending sqlstatement: " << sqlStatement << std::endl;
 }
+
