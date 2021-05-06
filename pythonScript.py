4aa47308420a5a16f3bc5d83cef29bcaa18b0d61
Your NameTal <taltulon123@gmail.com>
2021-05-07 02:47:49 +0300

    Fixed pythonScript

--- pythonScript.py
+++ pythonScript.py
@@ -1,14 +1,16 @@
 import socket
 import codecs
 
+
 if __name__ == '__main__':
     print("PyScript to test MT Server 1.0.1")
     userSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     userSocket.connect(("127.0.0.1", 5150))
     print(f'{201:08b}' + f'{37:032b}' + ''.join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\"}"))
-    userSocket.send(('' + f'{201:08b}' + f'{37:032b}'.join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\"}")).encode())
+    userSocket.send((f'{201:08b}' + f'{37:032b}' + ''.join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\"}")).encode())
     ans = userSocket.recv(1024).decode()
-    print(ans)
+    n = int(ans, 2)
+    print(n.to_bytes((n.bit_length() + 7) // 8, 'big').decode())
 
 
     #userSocket.send(''.join(format(202, '08b')).join(format(61, '32b')).join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\", mail: \"usr1@gmail.com\"}").encode())
