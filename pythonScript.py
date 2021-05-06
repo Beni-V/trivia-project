5120885428809eab271755550675472b0f26c99c
Your NameTal <taltulon123@gmail.com>
2021-05-06 14:57:27 +0300

    Updated th python script so it will send a real login or signup message

--- pythonScript.py
+++ pythonScript.py
@@ -1,8 +1,15 @@
 import socket
+import codecs
 
 if __name__ == '__main__':
     print("PyScript to test MT Server 1.0.1")
     userSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     userSocket.connect(("127.0.0.1", 5150))
-    userSocket.send("hello".encode())
-    print(userSocket.recv(1024).decode())
+    print(f'{201:08b}' + f'{37:032b}' + ''.join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\"}"))
+    userSocket.send(('' + f'{201:08b}' + f'{37:032b}'.join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\"}")).encode())
+    ans = userSocket.recv(1024).decode()
+    print(ans)
+
+
+    #userSocket.send(''.join(format(202, '08b')).join(format(61, '32b')).join(format(ord(i), '08b') for i in "{username: \"user1\", password: \"1234\", mail: \"usr1@gmail.com\"}").encode())
+    #print(userSocket.recv(1024).decode())
