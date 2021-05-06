18ca815bb5c21c564c2e8eb55350cd8607c44bf9
Beni Verner <binitto10@gmail.com>
2021-05-05 18:30:34 -0700

    sendSqlStatement method removed. (No need in that method)

--- SqliteDatabase.cpp
+++ SqliteDatabase.cpp
@@ -27,21 +27,10 @@ SqliteDatabase::SqliteDatabase()
     {
         // create users table
         std::string sqlStatement = "CREATE TABLE USERS (USERNAME TEXT, PASSWORD TEXT, EMAIL TEXT); ";
-        sendSqlStatement(sqlStatement, nullptr, nullptr);
+        char* errMessage = nullptr;
+        int res = sqlite3_exec(db, sqlStatement.c_str(), NULL, NULL, &errMessage); // execute statement
+        if (res != SQLITE_OK)
+            std::cout << "Trouble with sending sqlstatement: " << sqlStatement << std::endl;
 
     }
 }
-
-/*
-input: sqlStatement that should be executed, callback function (may be nullptr), data for output from callback (also may be nullptr)
-output: null
-function will execute sql statement and return result from callback to the third parameter if second and third parameters are not null
-*/
-void SqliteDatabase::sendSqlStatement(std::string sqlStatement, int(*callback)(void*, int, char**, char**), void* data)
-{
-    char* errMessage = nullptr;
-    int res = sqlite3_exec(db, sqlStatement.c_str(), callback, data, &errMessage); // execute statement
-    if (res != SQLITE_OK)
-        std::cout << "Trouble with sending sqlstatement: " << sqlStatement << std::endl;
-}
-
