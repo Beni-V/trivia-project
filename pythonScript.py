10bbccd79f86c87dfa60881853365dbed600dfdc
Tal Bodenheimer <taltulon123@gmail.com>
2021-05-13 16:28:38 +0000

    Revert "Merge branch 'Creating-RoomManager-2.0.0' into 'master'"
    This reverts merge request !12

--- pythonScript.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-if __name__ == '__main__':
-    print("PyScript to test MT Server 1.0.1")
-    userSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    userSocket.connect(("127.0.0.1", 5150))
-    userSocket.send("hello".encode())
-    print(userSocket.recv(1024).decode())
