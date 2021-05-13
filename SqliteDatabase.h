4ee720a4fe9219737e75903fae8f6960d8737b27
Beni Verner <binitto10@gmail.com>
2021-05-13 02:29:54 -0700

    All added methods added as abstract virtual methods in IDatabase class and defined as override methods in SqliteDatabase class

--- SqliteDatabase.h
+++ SqliteDatabase.h
@@ -15,11 +15,11 @@ public:
 	bool doesPasswordMatch(std::string userName, std::string password) override;
 	void addNewUser(std::string userName, std::string password, std::string email) override;
 	void sendSqlStatement(std::string sqlStatement, int(*callback)(void*, int, char**, char**), void* data);
-	std::vector<std::vector<std::string>> getQuestions();
-	float getPlayerAverageAnswerTime(std::string userName);
-	int getNumOfCorrectAnswers(std::string userName);
-	int getNumOfTotalAnswers(std::string userName);
-	int getNumOfPlayerGames(std::string userName);
+	std::vector<std::vector<std::string>> getQuestions() override;
+	float getPlayerAverageAnswerTime(std::string userName) override;
+	int getNumOfCorrectAnswers(std::string userName) override;
+	int getNumOfTotalAnswers(std::string userName) override;
+	int getNumOfPlayerGames(std::string userName) override;
 private:
 	sqlite3* db;
 };
