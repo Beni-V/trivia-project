a095f9585cce233115e0da5518be68269a83c318
Your NameTal <taltulon123@gmail.com>
2021-05-16 16:45:31 +0300

    Added to ^ConRequestPacketSerialization all the new requests

--- JsonResponsePacketSerializer.h
+++ JsonResponsePacketSerializer.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <bitset>
+#include "Room.h"
 
 struct LoginResponse
 {
@@ -19,10 +20,55 @@ struct ErrorResponse
 	std::string message;
 };
 
+struct LogoutResponse
+{
+	unsigned int status;
+};
+
+struct GetRoomsResponse
+{
+	unsigned int status;
+	std::vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse
+{
+	std::vector<std::string> players;
+};
+
+struct GetHighScoreResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct GetPersonalStatsResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct JoinRoomResponse
+{
+	unsigned int status;
+};
+
+struct CreateRoomResponse
+{
+	unsigned int status;
+};
+
 class JsonResponsePacketSerializer
 {
 public:
 	static std::vector<unsigned char> serializeResponse(LoginResponse LR);
 	static std::vector<unsigned char> serializeResponse(SignupResponse SR);
 	static std::vector<unsigned char> serializeResponse(ErrorResponse ER);
+	static std::vector<unsigned char> serializeResponse(LogoutResponse LR);
+	static std::vector<unsigned char> serializeResponse(GetRoomsResponse GRR);
+	static std::vector<unsigned char> serializeResponse(GetPlayersInRoomResponse GPIRR);
+	static std::vector<unsigned char> serializeResponse(GetHighScoreResponse GHSR);
+	static std::vector<unsigned char> serializeResponse(GetPersonalStatsResponse GPSR);
+	static std::vector<unsigned char> serializeResponse(JoinRoomResponse JRR);
+	static std::vector<unsigned char> serializeResponse(CreateRoomResponse CRR);
 };
