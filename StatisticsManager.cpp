17ffa61f69e3fe75e307f826c537701d33798f1c
Beni Verner <binitto10@gmail.com>
2021-05-17 10:16:16 -0700

    getUserStatistics() method in class StatisticsManager implemeted.

--- StatisticsManager.cpp
+++ StatisticsManager.cpp
@@ -2,10 +2,21 @@
 
 std::vector<std::string> StatisticsManager::getHighScore()
 {
-	return std::vector<std::string>();
+	std::vector<std::string> topPlayers;
+	int currPlayerTotalAnswers;
+	int currPlayerRightAnswers;
+	std::string currPlayerUserName;
+
 }
 
 std::vector<std::string> StatisticsManager::getUserStatistics(std::string userName)
 {
-	return std::vector<std::string>();
+	// gte the stats from DB
+	std::string playerAvegageAnswerTime = std::to_string(this->m_database->getPlayerAverageAnswerTime(userName));
+	std::string numOfCorrectAnswers = std::to_string(this->m_database->getNumOfCorrectAnswers(userName));
+	std::string numOfTotalAnswers = std::to_string(this->m_database->getNumOfTotalAnswers(userName));
+	std::string numOfPlayerGames = std::to_string(this->m_database->getNumOfPlayerGames(userName));
+
+	// return the stats in vector
+	return std::vector<std::string>({ playerAvegageAnswerTime , numOfCorrectAnswers, numOfTotalAnswers, numOfPlayerGames });
 }
