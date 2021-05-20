488a0491180be3583f5ab99e82ab175904dff05a
Beni Verner <binitto10@gmail.com>
2021-05-19 18:30:24 -0700

    RoomManager and StatisticsManager fields added to HandleRequestFactory

--- RequestHandlerFactory.h
+++ RequestHandlerFactory.h
@@ -3,6 +3,8 @@
 #include "LoginRequestHandler.h"
 #include "MenuRequestHandler.h"
 #include "LoginManager.h"
+#include "RoomManager.h"
+#include "StatisticsManager.h"
 
 class LoginRequestHandler;
 
@@ -15,5 +17,8 @@ public:
 
 private:
 	LoginManager m_loginManager;
+	RoomManager m_roomManager;
+	StatisticsManager m_StatisticsManager;
+
 	IDAtabase* m_database;
 };
