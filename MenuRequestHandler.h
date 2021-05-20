4509a1ab1d20f00b6115c9761ddbb359e53ffe62
Beni Verner <binitto10@gmail.com>
2021-05-19 20:32:20 -0700

    new fields added to menuRequestHander class

--- MenuRequestHandler.h
+++ MenuRequestHandler.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include "IRequestHandler.h"
+#include "LoggedUser.h"
+#include "RoomManager.h"
+#include "StatisticsManager.h"
+#include "RequestHandlerFactory.h"
 
 class MenuRequestHandler : public IRequestHandler
 {
+public:
 	bool isRequestRelevant(RequestInfo info) override;
 	RequestResult handleRequest(RequestInfo info) override;
+private:
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	StatisticsManager& m_statisticsManager;
+	RequestHandlerFactory& m_handlerFactory;
 };
