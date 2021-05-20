7287628d32f15c06df31de9c51b8fec948632256
Beni Verner <binitto10@gmail.com>
2021-05-19 18:46:44 -0700

    isRequestRelevant() method in MenuRequestHandler class implemented

--- MenuRequestHandler.cpp
+++ MenuRequestHandler.cpp
@@ -1,8 +1,28 @@
 #include "MenuRequestHandler.h"
 
+#define CREATE_ROOM_REQUEST 501
+#define GET_ROOMS_REQUEST 502
+#define GET_PLAYERS_IN_ROOM_REQUEST 503
+#define JOIN_ROOM_REQUEST 504
+#define GET_STATISTICS_REQUEST 505
+#define LOGOUT_REQUEST 506
+
 bool MenuRequestHandler::isRequestRelevant(RequestInfo info)
 {
-    return false;
+	// if request id is equal to one of the defined requests id, return true.
+	if (info.requestId == CREATE_ROOM_REQUEST ||
+		info.requestId == GET_ROOMS_REQUEST ||
+		info.requestId == GET_PLAYERS_IN_ROOM_REQUEST ||
+		info.requestId == JOIN_ROOM_REQUEST ||
+		info.requestId == GET_STATISTICS_REQUEST ||
+		info.requestId == LOGOUT_REQUEST)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 RequestResult MenuRequestHandler::handleRequest(RequestInfo info)
