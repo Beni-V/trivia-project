9665914aa32c5ff08788f89c0e9f366cc6bfba6b
Beni Verner <binitto10@gmail.com>
2021-05-26 15:56:35 -0700

    RoomAdminRequestHandler class added (methods not implemented yet)

---
+++ RoomAdminRequestHandler.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "IRequestHandler.h"
+#include "Room.h"
+#include "LoggedUser.h"
+#include "RoomManager.h"
+#include "RequestHandlerFactory.h"
+
+class RoomAdminRequestHandler : public IRequestHandler
+{
+private:
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+
+	RequestResult closeRoom(RequestInfo requestInfoStrust);
+	RequestResult startGame(RequestInfo requestInfoStrust);
+	RequestResult getRoomState(RequestInfo requestInfoStrust);
+
+public:
+	bool isRequestRelevant(RequestInfo requestInfoStruct);
+	RequestResult handleRequest(RequestInfo requestInfoStruct);
+
+};
