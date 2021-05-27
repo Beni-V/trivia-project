e4f16db8816a1397bc6b9eb966d641c3e1e0a597
Beni Verner <binitto10@gmail.com>
2021-05-26 17:24:08 -0700

    RoomMemberRequestHandler::handleRequest() implemented

--- RoomMemberRequestHandler.cpp
+++ RoomMemberRequestHandler.cpp
@@ -11,6 +11,17 @@ RequestResult RoomMemberRequestHandler::leaveRoom(RequestInfo requestInfoStruct)
 	return requestResultStruct;
 }
 
+RequestResult RoomMemberRequestHandler::getRoomState(RequestInfo requestInfoStruct)
+{
+	RequestResult requestResultStruct;
+
+	requestResultStruct.Buffer = JsonResponsePacketSerializer::serializeResponse(GetRoomStateResponse{ SUCCSESS_RESPONSE, this->m_room.getIsActive(), this->m_room.getAllUsers(), this->m_room.getQuestionsAmount(), this->m_room.getQuestionTimeOut() }); // fill buffer with serialized response
+	requestResultStruct.newHandler = this; // fill newHandler with next handler
+
+	return requestResultStruct;
+}
+
+// will check if request id is relevant for current state
 bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo requestInfoStruct)
 {
 	if (requestInfoStruct.requestId == LEAVE_ROOM_REQUEST || requestInfoStruct.requestId == GET_ROOM_STATE_REQUEST)
@@ -21,4 +32,23 @@ bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo requestInfoStruct)
 	{
 		return false;
 	}
+}
+
+// will call for the right handler of this state
+RequestResult RoomMemberRequestHandler::handleRequest(RequestInfo requestInfoStruct)
+{
+	RequestResult requestResultStruct;
+
+	switch (requestInfoStruct.requestId)
+	{
+	case LEAVE_ROOM_REQUEST:
+		requestResultStruct = this->leaveRoom(requestInfoStruct);
+		break;
+
+	case GET_ROOM_STATE_REQUEST:
+		requestResultStruct = this->getRoomState(requestInfoStruct);
+		break;
+	}
+
+	return requestResultStruct;
 }
