e43b58f75bc97e062187d2f0ed51c58f38bde72a
Beni Verner <binitto10@gmail.com>
2021-05-26 17:13:29 -0700

    class RoomMemberRequestHandler added (methods not implemented yet)

---
+++ RoomMemberRequestHandler.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "IRequestHandler.h"
+#include "Room.h"
+#include "LoggedUser.h"
+#include "RoomManager.h"
+#include "RequestHandlerFactory.h"
+#include "JsonRequestPacketDeserializer.h"
+#include "JsonResponsePacketSerializer.h"
+
+class RoomMemberRequestHandler : public IRequestHandler
+{
+private:
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+
+	RequestResult leaveRoom(RequestInfo requestInfoStruct);
+	RequestResult getRoomState(RequestInfo requestInfoStruct);
+
+public:
+	bool isRequestRelevant(RequestInfo requestInfoStruct);
+	RequestResult handleRequest(RequestInfo requestInfoStruct);
+
+};
