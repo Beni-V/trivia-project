327c69e177314a2b220d6ffea2a1ffd321b860d8
Beni Verner <binitto10@gmail.com>
2021-05-26 17:20:39 -0700

    RoomMemberRequestHandler::leaveRoom() method implemented

--- RoomMemberRequestHandler.cpp
+++ RoomMemberRequestHandler.cpp
@@ -1,5 +1,16 @@
 #include "RoomMemberRequestHandler.h"
 
+RequestResult RoomMemberRequestHandler::leaveRoom(RequestInfo requestInfoStruct)
+{
+	RequestResult requestResultStruct;
+
+	this->m_room.removeUser(this->m_user); // remove user from room
+	requestResultStruct.Buffer = JsonResponsePacketSerializer::serializeResponse(LeaveRoomResponse{SUCCSESS_RESPONSE}); // fill buffer with serialized response
+	requestResultStruct.newHandler = this->m_handlerFactory.createMenuRequestHandler(); // fill newHandler with next handler
+
+	return requestResultStruct;
+}
+
 bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo requestInfoStruct)
 {
 	if (requestInfoStruct.requestId == LEAVE_ROOM_REQUEST || requestInfoStruct.requestId == GET_ROOM_STATE_REQUEST)
