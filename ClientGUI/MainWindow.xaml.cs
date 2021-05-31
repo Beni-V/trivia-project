995c1187a2dd424a561dc4ec368580cccb0e39ce
Your NameTal <taltulon123@gmail.com>
2021-06-01 00:51:21 +0300

    Created client GUI

---
+++ ClientGUI/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Signup_Click(object sender, RoutedEventArgs e)
+        {
+            //SignupWindow signupWindow = new SignupWindow();
+            //signupWindow.Show();
+            this.Close();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameBox.Text;
+            string password = passwordBox.Text;
+            MenuWindow menuWindow = new MenuWindow();
+            menuWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ClientGUI/MenuWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+
+namespace ClientGUI
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JoinRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MyStatus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BestScore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
