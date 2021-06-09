32f6851e125ed4802dccdfabdb3fb31fe8c4b85f
Beni Verner <binitto10@gmail.com>
2021-06-09 09:06:09 -0700

    Game::submitAnswer() method implemented

--- Game.cpp
+++ Game.cpp
@@ -4,3 +4,15 @@ Question Game::getQuestionForUser(LoggedUser user)
 {
 	return this->m_players[user].currentQuestion;
 }
+
+void Game::submitAnswer(LoggedUser user, std::string answer)
+{
+	if (answer == this->m_players[user].currentQuestion.getCorrectAnswer())
+	{
+		this->m_players[user].correctAnswerCount++;
+	}
+	else
+	{
+		this->m_players[user].wrongAnswerCount++;
+	}
+}
